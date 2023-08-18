1c7069b5f20facf0748a292c467a4a4fc32bfc31
Your Name <you@example.com>
2023-08-18 02:12:50 +0100

    hide button

--- src/App.js
+++ src/App.js
@@ -40,42 +40,55 @@ function App() {
       var x = document.getElementById("cats");
       if (x.style.display === "none") {
         x.style.display = "block";
+        document.getElementById("hidec").innerHTML = "Hide Cats"
       } else {
-        x.style.display = "none";
+        x.style.display = "none";        
+        document.getElementById("hidec").innerHTML = "Show Cats"
+
       }
     }
 
     function myFunctiond() {
       var x = document.getElementById("dogs");
-
-        if (x.style.display === "none") {
+      if (x.style.display === "none") {
         x.style.display = "block";
-
-      } else{
+        document.getElementById("hided").innerHTML = "Hide Dogs"
+      } else {
         x.style.display = "none";
-      } 
+        document.getElementById("hided").innerHTML = "Show Dogs"
+      }
     }
 
 
       
   return (
-    <div className='App' id='full'>
-          <div id='cats' className='u'>
-            <h1 className="display-1"><b>Cats!</b></h1>
-            <div className='display-4'>{quote}</div>
-            <button class="btn btn-outline-secondary" onClick={getquote} >New fact!</button>
-            <button class="btn btn-outline-secondary" onClick={() => {myFunction()}} >Hide</button>
+    <div>
+      <div class="d-flex justify-content-around">
+          <button  id='hidec' class="btn btn-outline-secondary" onClick={() => {myFunction()}} >Hide Cats</button>
+          <button id='hided' class="btn btn-outline-secondary" onClick={() => {myFunctiond()}} >Hide Dogs</button>
+      </div>
+      <div className='App' id='full'>
+          <div class="position-absolute top-0 start-0">
           </div>
 
-          <div class="vr"></div>
+            <div id='cats' className='u'>
+              <h1 className="display-1"><b>Cats!</b></h1>
+              <div className='display-4'>{quote}</div>
+              <button className="btn btn-outline-secondary" onClick={getquote} >New fact!</button>
+            </div>
 
-          <div id='dogs' className='w'>
-            <h1  className="display-1 "><b>Dogs!</b></h1>
-            <div className='display-4 '>{quoted}</div>
-            <button className="btn btn-outline-secondary " onClick={getquoted} >New fact!</button>
-            <button className="btn btn-outline-secondary" onClick={() => {myFunctiond()}} >Hide</button>
-          </div>
+            <div class="vr"></div>
+
+            <div class="position-absolute top-0 end-0">
+            </div>
+
+            <div id='dogs' className='w'>
+              <h1  className="display-1 "><b>Dogs!</b></h1>
+              <div className='display-4 '>{quoted}</div>
+              <button className="btn btn-outline-secondary " onClick={getquoted} >New fact!</button>
+            </div>
             
+      </div>
     </div>
   );
 }
